be0b725b6ed84feea4cc4d05f95c171fd0beb06c
Taufiqzs <taufiqurrahmanzahrudinsutomo@gmail.com>
2024-02-14 23:05:06 +0700

    Menu
    Add menu, options,graphics, dan sounds

---
+++ options.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_graphics_b_pressed():
+	get_tree().change_scene_to_file("res://graphics.tscn")
+
+
+func _on_sound_b_pressed():
+	get_tree().change_scene_to_file("res://sounds.tscn")
+
+
+func _on_backtomenu_pressed():
+
+	get_tree().change_scene_to_file("res://menu.tscn")
