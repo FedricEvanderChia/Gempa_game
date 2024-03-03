92424de3290d407e78ca3de403e279e716ffb8d0
Fedric Evander Chia <92733782+FedricEvanderChia@users.noreply.github.com>
2024-03-03 23:20:03 +0700

    beras, tenda
    berasa teduh

--- Toko.gd
+++ Toko.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("AnimatedSprite2D").play("left")
+	get_node("AnimatedSprite2D").play("right")
 	get_node("Button").hide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,9 +12,15 @@ func  _physics_process(delta):
 
 
 func _on_body_entered(body):
-	print("im in da hood of dis shop boiii")
-	get_node("AnimatedSprite2D").play("right")
+	get_node("AnimatedSprite2D").play("left")
 	get_node("Button").show()
-
+	
+func _on_body_exited(body):
+	get_node("AnimatedSprite2D").play("right")
+	get_node("Button").hide()
+	
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Minigames/Sembako.tscn")
+
+
+
