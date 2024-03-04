223cad49731ef95edd6677c8b884714ca9f594a4
Fedric Evander Chia <92733782+FedricEvanderChia@users.noreply.github.com>
2024-03-05 00:37:02 +0700

    Minigame Sembako bisa deteksi item di dropbox

--- Object/object.gd
+++ Object/object.gd
@@ -3,6 +3,7 @@ class_name object
 
 var selected = false
 var initialPos: Vector2 
+var in_drop_box = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	initialPos = global_position
@@ -22,7 +23,10 @@ func _on_area_2d_input_event(viewport, event, shape_idx):
 		else:
 			selected = false
 			position = initialPos
-			
+			if in_drop_box:
+				print("tambah 1 ",name, position)
+			else:
+				print(name,"-nya ga masuk bego")
 func _on_area_2d_mouse_entered():
 	scale = Vector2 (1.05,1.05)
 
@@ -31,8 +35,9 @@ func _on_area_2d_mouse_exited():
 
 func _on_area_2d_body_entered(body): 
 	body.modulate = Color(Color.REBECCA_PURPLE,1)
-	prints("tambah 1",name,position)
+	in_drop_box = true
+	
 	
 func _on_area_2d_body_exited(body):
 	body.modulate = Color(Color.MEDIUM_PURPLE,0.7)
-	prints("gajadi,",name,"ga masuk",position)
+	in_drop_box = false
