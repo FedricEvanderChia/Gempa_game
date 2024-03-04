6da83a23d784901904cb20b82800267c6ff8adfa
Taufiqzs <taufiqurrahmanzahrudinsutomo@gmail.com>
2024-03-04 19:23:50 +0700

    update gui

---
+++ UIGempa.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+# untuk pergerakan player, hanya tinggal memanggil action ui_left
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
