74e3d31534197e517be4e0b7c2a52500c75c9fc0
Taufiqzs <taufiqurrahmanzahrudinsutomo@gmail.com>
2024-03-26 17:23:25 +0700

    Update

--- menus/menu.gd
+++ menus/menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_b_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://Intro/Intro1.tscn")
 
 
 func _on_option_b_pressed():
